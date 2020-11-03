cbd975e5d864d1c3fe359faa842799840c74b4cb
BeNcH <slava.morzhakov@mail.ru>
2020-11-03 08:45:50 +0300

    Добавление привет мир

---
+++ Lab4_equation/Lab4_equation/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4_equation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Hello world";
+        }
+    }
+}
